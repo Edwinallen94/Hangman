15409b41a9250d61d49cd29b5b1e1ddf6130f50d
Edwin <Edwinallen94@aol.com>
2023-11-23 00:46:54 -0500

    added animation and bootstrap

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
+
+const readlineSync = require("readline-sync");
+const wordBank = require("./word-bank.js");
+
+// Main game loop
+const playHangman = () => {
+  console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
+
+  const wordToGuess = getRandomWord(wordBank);
+  let displayWord = initializeDisplay(wordToGuess);
+  let incorrectGuesses = 0;
+  let guessedLetters = [];
+};
